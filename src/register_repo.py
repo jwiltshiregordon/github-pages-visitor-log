f54d2761e305864cc0202dc69760cf80c30fbe40
John Wiltshire-Gordon <jwiltshiregordon@gmail.com>
2023-09-25 09:38:49 -0400

    register/unregister logic

--- src/register_repo.py
+++ src/register_repo.py
@@ -1,17 +1,29 @@
 import boto3
 import requests
-from .constants import AWS_S3_BUCKET, REGISTERED_PATH, LOGS_PATH, CHALLENGE_FILENAME
+from .constants import AWS_S3_BUCKET, REGISTERED_PATH, LOGS_PATH, CHALLENGE_FILENAME  # Adjust the import as needed
 
 
 def register_repo(repo_name, repo_owner):
+    s3 = boto3.client("s3")
     blob_url = f"https://github.com/{repo_owner}/{repo_name}/blob/main/{CHALLENGE_FILENAME}"
     r = requests.get(blob_url)
+
+    registered_key = f"{REGISTERED_PATH}{repo_name}"
+    log_key = f"{LOGS_PATH}{repo_name}"
+
+    response = {}
+
     if r.status_code == 404:
-        return f"File {CHALLENGE_FILENAME} not found in repository"
+        # Delete keys if they exist
+        s3.delete_object(Bucket=AWS_S3_BUCKET, Key=registered_key)
+        s3.delete_object(Bucket=AWS_S3_BUCKET, Key=log_key)
+        response["status"] = "unregistered"
+        response["message"] = f"File {CHALLENGE_FILENAME} not found in repository. Unregistered the repo."
+    else:
+        # Create or update keys
+        s3.put_object(Bucket=AWS_S3_BUCKET, Key=registered_key, Body="")
+        s3.put_object(Bucket=AWS_S3_BUCKET, Key=log_key, Body="")
+        response["status"] = "registered"
+        response["message"] = "Successfully registered the repository."
 
-    s3 = boto3.client("s3")
-
-    s3.put_object(Bucket=AWS_S3_BUCKET, Key=f"{REGISTERED_PATH}{repo_name}", Body="")
-    s3.put_object(Bucket=AWS_S3_BUCKET, Key=f"{LOGS_PATH}{repo_name}", Body="")
-
-    return "Successfully registered"
+    return response
--- tests/test_register_repo.py
+++ tests/test_register_repo.py
@@ -3,7 +3,7 @@ import pytest
 from moto import mock_s3
 from unittest.mock import patch
 from src.register_repo import register_repo
-from src.constants import CHALLENGE_FILENAME
+from src.constants import AWS_S3_BUCKET, CHALLENGE_FILENAME
 
 FAKE_AWS_CREDENTIALS = {
     "aws_access_key_id": "fake_access_key",
@@ -11,11 +11,12 @@ FAKE_AWS_CREDENTIALS = {
     "aws_session_token": "fake_session_token",
 }
 
+
 @pytest.fixture
 def s3():
     with mock_s3():
         conn = boto3.client("s3", **FAKE_AWS_CREDENTIALS)
-        conn.create_bucket(Bucket="my_test_bucket")
+        conn.create_bucket(Bucket=AWS_S3_BUCKET)
         yield conn
 
 
@@ -23,13 +24,23 @@ def s3():
 def test_register_repo_with_valid_file(mock_requests_get, s3):
     mock_requests_get.return_value.status_code = 200
     result = register_repo("some_repo_name", "some_repo_owner")
-    assert result == "Successfully registered"
+
+    assert result['status'] == 'registered'
+
     mock_requests_get.assert_called_with(f"https://github.com/some_repo_owner/some_repo_name/blob/main/{CHALLENGE_FILENAME}")
+
+    response = s3.list_objects_v2(Bucket=AWS_S3_BUCKET, Prefix='registered/some_repo_name')
+    assert 'Contents' in response
 
 
 @patch('src.register_repo.requests.get')
 def test_register_repo_with_missing_file(mock_requests_get, s3):
     mock_requests_get.return_value.status_code = 404
     result = register_repo("some_repo_name", "some_repo_owner")
-    assert result != "Successfully registered"
+
+    assert result['status'] == 'unregistered'
+
     mock_requests_get.assert_called_with(f"https://github.com/some_repo_owner/some_repo_name/blob/main/{CHALLENGE_FILENAME}")
+
+    response = s3.list_objects_v2(Bucket=AWS_S3_BUCKET, Prefix='registered/some_repo_name')
+    assert 'Contents' not in response
